a4e1055d8245e7b336b05d993107aebdfbdb6d6f
dhankhar0506 <dhankhargaurav51@gmail.com>
2023-04-27 10:10:46 +0530

    counterapp

---
+++ 27-04/counter-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Updater } from './component/Updater';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Updater/>
+    </div>
+  );
+}
+
+export default App;
